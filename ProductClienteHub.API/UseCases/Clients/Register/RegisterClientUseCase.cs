c31dc79685c89aa9b1ac6f5d6930a3aa060e4689
Lucas Bubanz <lucas.bubanz@herval.com.br>
2025-05-09 15:51:26 -0300

    Remove unused using directives from RegisterClientUseCase

--- ProductClienteHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
+++ ProductClienteHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ProductClienteHub.Communication.Requests;
 using ProductClienteHub.Communication.Responses;
 using ProductClienteHub.Execptions.ExceptionsBase;
